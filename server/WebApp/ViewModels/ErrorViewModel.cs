a1e5793cb3f58269e55bb5835cfa0b6b9bb346d5
Roma Tariq <roma.imran@gmail.com>
2023-11-22 17:13:16 +0200

    Remove views and controllers

--- server/WebApp/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-#pragma warning disable 1591
-namespace WebApp.ViewModels;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
